e4df10c53b2c158e03c0f8612e6f1560dc3801f8
BuildTools <unconfigured@null.spigotmc.org>
2021-07-27 17:47:20 +0200

    Update Base Work

--- index.php
+++ index.php
@@ -10,19 +10,30 @@ foreach ($envcontents as $line) if($line)
         if(sizeof($split) < 2)
             continue;
 
-        define($split[0], substr($line, strlen($split[0]) + 1));
+        $_ENV[$split[0]] = substr($line, strlen($split[0]) + 1);
         unset($line, $split);
     }
 
 unset($envcontents);
 
-
 ?><!DOCTYPE html>
 <html lang="fr">
     <head>
-        <title>Reservia</title>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title><?= $_ENV["APPNAME"] ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="<?= $_ENV["BASEURL"] ?>style.css" />
     </head>
     <body>
+        <header>
 
+        </header>
+        <main>
+
+        </main>
+        <footer>
+
+        </footer>
     </body>
 </html>
