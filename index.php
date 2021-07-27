a3c1f4eaf47b88a9c31a3d83ed87c79b97c89337
BuildTools <unconfigured@null.spigotmc.org>
2021-07-27 16:38:34 +0200

    Make Environement

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
-<!DOCTYPE html>
+<?php
+include_once "dbutils_val.php";
+
+$envcontents = explode("\r", file_get_contents("./.env"));
+
+foreach ($envcontents as $line) if($line)
+    {
+        $split = explode("=", $line);
+
+        if(sizeof($split) < 2)
+            continue;
+
+        define($split[0], substr($line, strlen($split[0]) + 1));
+        unset($line, $split);
+    }
+
+unset($envcontents);
+
+
+?><!DOCTYPE html>
 <html lang="fr">
     <head>
         <title>Reservia</title>
