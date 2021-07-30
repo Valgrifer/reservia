9f9981d7966c3621cb7336c290b7858d56afdccb
BuildTools <unconfigured@null.spigotmc.org>
2021-07-30 14:04:36 +0200

    Header Téléphone

--- index.php
+++ index.php
@@ -2,18 +2,43 @@
 include_once "dbutils_val.php";
 include_once "utils.php";
 
+$nav = [
+    new nav_items("Hébergements", "#hebergements"),
+    new nav_items("Activités", "#activites")
+];
+
 ?><!DOCTYPE html>
 <html lang="fr">
     <head>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title><?= $_ENV["APPNAME"] ?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title><?= $_ENV["APPNAME"] ?></title>
+
+        <link rel="preload" href="<?= url("font/Raleway-VariableFont_wght.ttf") ?>" as="font" type="font/ttf" crossorigin>
+
         <link rel="stylesheet" type="text/css" media="screen" href="<?= url("style.css") ?>" />
     </head>
     <body>
         <header>
-
+            <span class="logo">
+                <a href="<?= url("#") ?>">
+                    <img src="<?= url($_ENV["APPLOGO"]) ?>" alt="<?= $_ENV["APPNAME"] ?> Logo">
+                </a>
+            </span>
+            <nav>
+                <ul>
+<?php
+for ($x = 0; $x < sizeof($nav); $x++)
+{
+    ?>                    <li><a href="<?= url($nav[$x]->getSlug()) ?>"<?= $x === 0 ? " class=\"active\"" : "" ?>><?= $nav[$x]->getName() ?></a></li>
+<?php } ?>
+                </ul>
+                <span class="register">
+                    <a href="<?= url("#") ?>">S'inscrire</a>
+                </span>
+            </nav>
         </header>
         <main>
 
--- utils.php
+++ utils.php
@@ -4,6 +4,7 @@ $envcontents = explode("\n", file_get_contents("./.env"));
 
 foreach ($envcontents as $line) if($line)
 {
+    $line = str_replace(["\n", "\r"], "", $line);
     $split = explode("=", $line);
 
     if(sizeof($split) < 2)
@@ -21,4 +22,40 @@ function str_ends_with ($handle, $str)
 function url($url)
 {
     return $_ENV["BASEURL"] . (!str_ends_with("/", $_ENV["BASEURL"]) ? "/" : "") . $url;
+}
+
+class nav_items
+{
+    private $name;
+    private $slug;
+
+    /**
+     * nav_items constructor.
+     * @param $name
+     * @param $slug
+     */
+    public function __construct($name, $slug = null)
+    {
+        $this->name = $name;
+        if($slug)
+            $this->slug = $slug;
+        else
+            $this->slug = strtolower($name);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
 }
